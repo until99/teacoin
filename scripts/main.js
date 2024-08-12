6c504838d5fbedc1dc6c06e0fb3d344e6ec15cb0
untill99 <gabriel.kasten@outlook.com>
2024-08-12 00:28:39 -0300

    criação de task ok

--- scripts/main.js
+++ scripts/main.js
@@ -2,42 +2,56 @@
 let task_steps_wrapper = document.getElementById("task_steps_wrapper");
 let create_task_step_button = document.getElementById("task_create_new_step_button");
 
-// Step counter
 let step_id = 1;
 
 function taskCreateStep(step_id) {
   let step_html = document.createElement('div');
   step_html.setAttribute('class', 'step');
-  step_html.setAttribute('id', step_id);
+  step_html.setAttribute('id', `step_${step_id}`);
 
   let step_title_label = document.createElement('label');
   step_title_label.setAttribute('class', 'step_title_label');
   step_title_label.setAttribute('id', 'step_title_label');
-  step_title_label.appendChild(document.createTextNode(`Etapa ${step_id}`))  
-  
+  step_title_label.appendChild(document.createTextNode(`Etapa ${step_id}`));
 
   let step_title_input_html = document.createElement('input');
   step_title_input_html.setAttribute('class', 'step_title_input');
-  step_title_input_html.setAttribute('id', 'step_title');
+  step_title_input_html.setAttribute('id', `step_title_${step_id}`);
   step_title_input_html.setAttribute('type', 'text');
-  step_title_input_html.setAttribute('name', 'step_title');
+  step_title_input_html.setAttribute('name', `step_title_${step_id}`);
   step_title_input_html.setAttribute('placeholder', 'Nome da Etapa');
 
   let step_description_textarea_html = document.createElement('textarea');
   step_description_textarea_html.setAttribute('class', 'step_description_textarea');
-  step_description_textarea_html.setAttribute('id', 'step_description');
-  step_description_textarea_html.setAttribute('name', 'step_description');
+  step_description_textarea_html.setAttribute('id', `step_description_${step_id}`);
+  step_description_textarea_html.setAttribute('name', `step_description_${step_id}`);
   step_description_textarea_html.setAttribute('placeholder', 'Descreva a Etapa');
+
+  let step_remove_button_html = document.createElement('button');
+  step_remove_button_html.setAttribute('class', 'steo_remove_button');
+  step_remove_button_html.setAttribute('id', `step_remove_button_${step_id}`);
+  step_remove_button_html.setAttribute('name', `step_remove_button_${step_id}`);
+  step_remove_button_html.setAttribute('onclick', `stepRemove(${step_id})`);
+  step_remove_button_html.appendChild(document.createTextNode('remove'))
 
   step_html.appendChild(step_title_label);
   step_html.appendChild(step_title_input_html);
   step_html.appendChild(step_description_textarea_html);
+  step_html.appendChild(step_remove_button_html);
 
   task_steps_wrapper.appendChild(step_html);
 }
 
-// Add new step to task event
+// TODO:
+// Limitar a 5
 create_task_step_button.addEventListener("click", () => {
   taskCreateStep(step_id);
-  step_id += 1;
+  step_id++;
+  console.log(step_id);
 })
+
+// TODO:
+// Ajustar numeração dinamicamente
+function stepRemove(step_id) {
+  document.getElementById(`step_${step_id}`).remove();
+}
--- scripts/tasks/create_task.js
+++ scripts/tasks/create_task.js
@@ -1,12 +1,30 @@
 const pb = new PocketBase('https://strechout.pockethost.io');
 
+// TODO:
+// Não deixar enviar nenhum campo vazio
+// Indicador visual
 document.querySelector('#task_form').addEventListener('submit', (e) => {
   e.preventDefault();
+
   const data = Object.fromEntries(new FormData(e.target).entries());
 
-  console.log(data);
-  
-  // createNewTask(data);
+  let data_formated = {
+    task_title: data.task_title,
+    task_description: data.task_description,
+    steps: [],
+    task_start_time: formatedDateTime(data.start_time)
+  };
+
+  let step_index = 1;
+  while (data[`step_title_${step_index}`] && data[`step_description_${step_index}`]) {
+    data_formated.steps.push({
+      title: data[`step_title_${step_index}`],
+      description: data[`step_description_${step_index}`]
+    });
+    step_index++;
+  };
+
+  createNewTask(data_formated);
 })
 
 async function createNewTask(data) {
@@ -14,7 +32,15 @@ async function createNewTask(data) {
     await pb.collection('tasks').create(data);
     console.log('Atividade criada');
   }
-  catch {
+  catch (error) {
     console.error('Erro ao criar atividade:', error);
   }
+}
+
+function formatedDateTime(date_time) {
+  let dt = new Date(date_time);
+  let tzoffset = (new Date()).getTimezoneOffset() * 60000;
+  let localISOTime = (new Date(dt - tzoffset)).toISOString().slice(0,-5).replace("T", " ");
+
+  return localISOTime;
 }
